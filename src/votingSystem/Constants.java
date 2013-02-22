71c0ba1752b31195e422381accbb5fdc8a6481aa
Benjamin Jaeger <bjaeger2@gmail.com>
2013-02-22 17:18:03 -0500

    changing location of constants

---
+++ src/votingSystem/Constants.java
@@ -0,0 +1,7 @@
+package votingSystem.cTF;
+
+public final class Constants {
+	public static final int PORT = 9001;
+	public static final int POOL_THREADS = 10;
+	public static final int MESSAGE_SIZE = 2048;
+}
