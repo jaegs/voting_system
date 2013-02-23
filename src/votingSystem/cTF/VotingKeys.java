d75c9e2a91b630a7ffe50f1f3c50a3ced41f3bf4
lenardot <tel36@cornell.edu>
2013-02-23 13:37:28 -0500

    Create VotingKeys.java
    Class that implements the "oblivious transfer" aspect of the project to get unique voting keys to all of the voters.

---
+++ src/votingSystem/cTF/VotingKeys.java
@@ -0,0 +1,21 @@
+package votingSystem.cTF;
+
+import java.util.*;
+
+public class VotingKeys {
+
+    private HashSet<String> keys;
+    
+    public VotingKeys(){
+        
+        keys = null;
+    }
+    
+    public generateKeySet(){
+        
+        
+    }
+    
+    
+
+}
