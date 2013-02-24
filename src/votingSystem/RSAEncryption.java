b1e79751682fb0ecf02e6a9a3cef92a651135720
lenardot <tel36@cornell.edu>
2013-02-24 01:16:44 -0500

    Update src/votingSystem/RSAEncryption.java
    Written RSA

--- src/votingSystem/RSAEncryption.java
+++ src/votingSystem/RSAEncryption.java
@@ -1,5 +1,33 @@
 package votingSystem;
 
-public class RSAEncryption {
+import java.math.BigInteger;
+import java.security.SecureRandom;
 
+
+//This is plagurized (by tim) from a website I found online. We shouldn't use it, but it could guide us nicely. 
+
+class RSAEncryption
+{
+  private BigInteger n, d, e;
+
+  public Rsa(int bitlen)
+  {
+    SecureRandom r = new SecureRandom();
+    BigInteger p = new BigInteger(bitlen / 2, 100, r);
+    BigInteger q = new BigInteger(bitlen / 2, 100, r);
+    n = p.multiply(q);
+    BigInteger m = (p.subtract(BigInteger.ONE))
+                   .multiply(q.subtract(BigInteger.ONE));
+    e = new BigInteger("3");
+    while(m.gcd(e).intValue() > 1) e = e.add(new BigInteger("2"));
+    d = e.modInverse(m);
+  }
+  public BigInteger encrypt(BigInteger message)
+  {
+    return message.modPow(e, n);
+  }
+  public BigInteger decrypt(BigInteger message)
+  {
+    return message.modPow(d, n);
+  }
 }
