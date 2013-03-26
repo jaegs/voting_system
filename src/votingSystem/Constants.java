27682f635def75d8208825f6b2568fee06da5f57
Clover <crb89@cornell.edu>
2013-03-26 13:29:47 -0400

    Added encryption constants.

--- src/votingSystem/Constants.java
+++ src/votingSystem/Constants.java
@@ -1,6 +1,9 @@
 package votingSystem;
 
+import java.security.PublicKey;
+
 public final class Constants {
+	// Voting Protocol Constants
 	public static enum Operation {
 		NOOP, //Zero value operation gets chopped off by RSA
 		ISELIGIBLE,
@@ -19,12 +22,19 @@ public final class Constants {
 		OTGETSECRETS
 	}
 	public static final Operation[] OPERATION_VALUES = Constants.Operation.values();
-	//Server
+	
+	// Server Constants
 	public static final int PORT = 9001;
 	public static final int POOL_THREADS = 10;
 	public static final int MESSAGE_SIZE = 2048;
 	public static final int PASSWORD_LENGTH = 50; //Length of BigInteger
 	public static final String HOST = null;
 	public static final String CTF_PUBLIC_KEY = null;
-	//Client
+	
+	// Client Constants
+	
+	// Encryption Constants
+	public static final int RSA_KEY_SIZE = 2048;
+	public static final int AES_KEY_SIZE = 128;
+	public static PublicKey ctfpublic;
 }
