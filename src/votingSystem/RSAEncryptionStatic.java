9404df4c68567448945b1aab186a8bfb92549b36
lenardot <tel36@cornell.edu>
2013-03-26 14:18:49 -0300

    Update RSAEncryptionStatic.java

--- src/votingSystem/RSAEncryptionStatic.java
+++ src/votingSystem/RSAEncryptionStatic.java
@@ -65,16 +65,23 @@ public class RSAEncryptionStatic {
 				curEncrypt[y] = toEncrypt[(i*length) + y]; 				
 			}
 			
+			System.out.println("First to Encrypt: ");
+			RSAEncryptionStatic.printByteArray(curEncrypt);
+			
 			//encrypt the chunk!
 			Cipher cipher = Cipher.getInstance("RSA"); 
 		    cipher.init(Cipher.ENCRYPT_MODE, pub);
 		    byte[] encrypted = cipher.doFinal(curEncrypt);
 		    
+		    System.out.println("First Encrypted: ");
+			RSAEncryptionStatic.printByteArray(encrypted);
+			System.out.println();
+		    
 		    System.out.println("LENGTH: " + encrypted.length);
 		    
 		    //copy data into the array to return
-		    for(int y=0; y < length; y++){
-				toRet[(i*length) + y] = curEncrypt[y]; 				
+		    for(int y=0; y < (length+11); y++){
+				toRet[(i*(length+11)) + y] = encrypted[y]; 				
 			}
 		}
 		
@@ -91,12 +98,18 @@ public class RSAEncryptionStatic {
 				}
 			}
 			
+			System.out.println("Second to Encrypt: ");
+			RSAEncryptionStatic.printByteArray(curEncrypt);
+			
 			//encrypt the final chunk
 			Cipher cipher = Cipher.getInstance("RSA"); 
 		    cipher.init(Cipher.ENCRYPT_MODE, pub);
 		    byte[] encrypted = cipher.doFinal(curEncrypt);
 		    
-		    System.out.println("LENGTH: " + encrypted.length);
+		    System.out.println("Second Encrypted: ");
+			RSAEncryptionStatic.printByteArray(encrypted);
+			System.out.println();
+		    
 		    //copy encrypted data into the array to return
 		    for(int y=0; y < encrypted.length; y++){
 				toRet[(split*encrypted.length) + y] = encrypted[y]; 				
@@ -122,11 +135,11 @@ public class RSAEncryptionStatic {
 		
 
 		//used for determining padding/splits
-		int split = toDecrypt.length / length;
-		int remainder = toDecrypt.length % length;
+		int split = toDecrypt.length / (length + 11);
+		int remainder = toDecrypt.length % (length + 11);
 		
 		//new chunk of correct size
-		byte[] curEncrypt = new byte[length];
+		byte[] curEncrypt = new byte[(length + 11)];
 		
 		//if there is not an even number of blocks, throw an exception
 		if(remainder > 0){
@@ -136,15 +149,18 @@ public class RSAEncryptionStatic {
 		else{
 			
 			//the byte array to  be returned!
-			byte[] toRet = new byte[(length - 11) * split];
+			byte[] toRet = new byte[(length) * split];
 			
 			//perform encryption for all complete chunks
 			for(int i = 0; i < split; i++){
 				
 				//copy data over!
-				for(int y=0; y < length; y++){
-					curEncrypt[y] = toDecrypt[(i*length) + y]; 				
+				for(int y=0; y < (length + 11); y++){
+					curEncrypt[y] = toDecrypt[(i*(length + 11)) + y]; 				
 				}
+				
+				System.out.println("First to Decrypt: ");
+				RSAEncryptionStatic.printByteArray(curEncrypt);
 				
 				Cipher cipher = Cipher.getInstance("RSA");
 		        cipher.init(Cipher.DECRYPT_MODE, priv);
@@ -172,9 +188,25 @@ public class RSAEncryptionStatic {
 	}
 	
 	public static void main(String args[]) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
-		RSAEncryption rsa = new RSAEncryption(1024);
-		PublicKey pub = rsa.getPublic();
-		PrivateKey priv = rsa.getPrivate();
+		RSAEncryption rsa1 = new RSAEncryption(1024);
+		PublicKey pub1 = rsa1.getPublic();
+		PrivateKey priv1 = rsa1.getPrivate();
+		
+		RSAEncryption rsa2 = new RSAEncryption(2048);
+		PublicKey pub2 = rsa2.getPublic();
+		PrivateKey priv2 = rsa2.getPrivate();
+		
+		RSAEncryption rsa3 = new RSAEncryption(4096);
+		PublicKey pub3 = rsa3.getPublic();
+		PrivateKey priv3 = rsa3.getPrivate();
+		
+		System.out.println("Pub1 Length: " + pub1.getEncoded().length);
+		System.out.println("Priv1 Length: " + priv1.getEncoded().length);
+		System.out.println("Pub2 Length: " + pub2.getEncoded().length);
+		System.out.println("Priv2 Length: " + priv2.getEncoded().length);
+		System.out.println("Pub3 Length: " + pub3.getEncoded().length);
+		System.out.println("Priv3 Length: " + priv3.getEncoded().length);
+		
 		
 		/*byte[] msg = new byte[5];
 		msg[0] = 'a';
@@ -183,7 +215,7 @@ public class RSAEncryptionStatic {
 		msg[3] = 'm';
 		msg[4] = 'l';*/
 		
-		byte[] msg = new byte[200];
+		/*byte[] msg = new byte[200];
 		for(int z = 0; z< msg.length; z++){
 			msg[z] = 'a';
 		}
@@ -193,17 +225,20 @@ public class RSAEncryptionStatic {
 		RSAEncryptionStatic.printByteArray(msg);
 		
 		byte[] msg2 = RSAEncryptionStatic.encrypt(pub, msg, rsa.maxLength());
+		
 		System.out.println(msg2.length);
+		
+		RSAEncryptionStatic.printByteArray(msg2);
 		
 		//msg3 = rsa.decrypt(msg2);
 		
-		msg3 = RSAEncryptionStatic.decrypt(priv, msg2, rsa.maxLength()+ 11);
+		msg3 = RSAEncryptionStatic.decrypt(priv, msg2, rsa.maxLength());
 		
 		System.out.print("MSG: ");
 		RSAEncryptionStatic.printByteArray(msg);
 		System.out.print("MSG2: ");
 		RSAEncryptionStatic.printByteArray(msg2);
 		System.out.print("MSG3: ");
-		RSAEncryptionStatic.printByteArray(msg3);
+		RSAEncryptionStatic.printByteArray(msg3);*/
 	}
 }
