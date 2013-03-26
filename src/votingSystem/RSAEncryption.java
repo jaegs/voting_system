0b46975242a5a1914beb2553f192aa707ce52082
Clover <crb89@cornell.edu>
2013-03-26 13:26:58 -0400

    Updated RSAEncryption to be as general as possible for reuse in multiple situations.

--- src/votingSystem/RSAEncryption.java
+++ src/votingSystem/RSAEncryption.java
@@ -9,55 +9,33 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.InvalidKeyException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 /**
  * RSA Encryption
  * @author Clover
  */
 public class RSAEncryption {
-	// rawr
-	private int keysize;
-	private KeyPair keys;
-	
-	public RSAEncryption(int size)  
-	{
-		KeyPairGenerator keygen;
-		try {
-			keygen = KeyPairGenerator.getInstance("RSA");
-			keygen.initialize(size);
-			keysize = size;
-			keys = keygen.genKeyPair();
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}		
+		
+	public RSAEncryption() {
 	}
+		
+	public KeyPair genKeys() throws NoSuchAlgorithmException {
+		KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+		keygen.initialize(Constants.RSA_KEY_SIZE);
+		return keygen.genKeyPair();
+}
 	
-	public PublicKey getPublic(){
-		return keys.getPublic();
-	}
-	
-	public PrivateKey getPrivate(){
-		return keys.getPrivate();
-	}
-	
-	
-	public int maxLength() {
-		return (keysize/8) - 11;
-	}
-	
-	public int messageSize() {
-		return keysize/8;
-	}
-	
-	public byte[] encrypt(byte[] msg) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+	public byte[] encrypt(byte[] msg, PublicKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		 Cipher cipher = Cipher.getInstance("RSA"); 
-	     cipher.init(Cipher.ENCRYPT_MODE, keys.getPublic());
+	     cipher.init(Cipher.ENCRYPT_MODE, key);
 	     return cipher.doFinal(msg);
 	}
 	
-	public byte[] decrypt(byte[] msg) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+	public byte[] decrypt(byte[] msg, PrivateKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, keys.getPrivate());
+        cipher.init(Cipher.DECRYPT_MODE, key);
         return cipher.doFinal(msg);
 	}
 }	
