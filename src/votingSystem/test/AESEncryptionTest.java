8869945992ee097ae0eea22528f6eb955cc94d19
Clover <crb89@cornell.edu>
2013-03-29 19:11:41 -0400

    Updated.

--- src/votingSystem/test/AESEncryptionTest.java
+++ src/votingSystem/test/AESEncryptionTest.java
@@ -6,7 +6,6 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.InvalidParameterSpecException;
 import java.util.Arrays;
 
@@ -22,12 +21,10 @@ import votingSystem.RSAEncryption;
 public class AESEncryptionTest {
 	@Test	
 	public void testEncryptDecrypt() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidParameterSpecException {
-		RSAEncryption RSA = new RSAEncryption();
-		KeyPair keys = RSA.genKeys();
+		KeyPair keys = RSAEncryption.genKeys();
 		String s = "I am testing a long string to see if AES works, because if it doesn't, I will cry very very much. Please work. Don't make me cry.";
-		AESEncryption AES = new AESEncryption();
-		byte[] msgEnc = AES.encrypt(s.getBytes(), keys.getPublic());
-		byte[] msgDec = AES.decrypt(msgEnc, keys.getPrivate());
+		byte[] msgEnc = AESEncryption.encrypt(s.getBytes(), keys.getPublic());
+		byte[] msgDec = AESEncryption.decrypt(msgEnc, keys.getPrivate());
 		assertTrue(Arrays.equals(s.getBytes(),msgDec));						
 	}
 }
