bd730a14c19f4894147f6dca4ea151c322b74568
Clover <crb89@cornell.edu>
2013-03-29 14:33:44 -0400

    Undoing the null CTF_PUBLIC_KEY. It shouldn't be null.

--- src/votingSystem/Constants.java
+++ src/votingSystem/Constants.java
@@ -5,22 +5,19 @@ import java.security.PublicKey;
 public final class Constants {
 
 	public static final Operation[] OPERATION_VALUES = Operation.values();
-	//Server
+
+	//Server Constants
 	public static final int PORT = 9001;
 	public static final int POOL_THREADS = 10;
 	public static final int MESSAGE_SIZE = 2048;
 	public static final int PASSWORD_LENGTH = 50; //Length of BigInteger
 	public static final String HOST = null;
-	public static final String CTF_PUBLIC_KEY = null;
 	public static final String TEMPLATE_FILENAME = "blah";
-	//Client
 	
 	public static enum VoteStatus {SUCCESS,ID_COLLISION, NOT_RECORDED, INVALID_ELECTION_STATE};
-	
-	// Client Constants
-	
+		
 	// Encryption Constants
 	public static final int RSA_KEY_SIZE = 2048;
 	public static final int AES_KEY_SIZE = 128;
-	//public static PublicKey CTF_PUBLIC_KEY;
+	public static PublicKey CTF_PUBLIC_KEY;
 }
