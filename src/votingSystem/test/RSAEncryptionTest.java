1569baef4b893f1b4b17359040fbcd61b3738656
Clover <crb89@cornell.edu>
2013-03-29 19:37:44 -0400

    Added test of decrypt encrypt

--- src/votingSystem/test/RSAEncryptionTest.java
+++ src/votingSystem/test/RSAEncryptionTest.java
@@ -22,4 +22,11 @@ public class RSAEncryptionTest {
 		byte[] msg = s.getBytes();
 		assertTrue(Arrays.equals(msg,RSAEncryption.decrypt(RSAEncryption.encrypt(msg, keys.getPublic()),keys.getPrivate())));
 	}
+	@Test
+	public void testDecryptEncrypt() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+		KeyPair keys = RSAEncryption.genKeys();
+		String s = "Hello, World!";
+		byte[] msg = s.getBytes();
+		assertTrue(Arrays.equals(msg,RSAEncryption.encrypt(RSAEncryption.decrypt(msg, keys.getPublic()),keys.getPrivate())));
+	}
 }
