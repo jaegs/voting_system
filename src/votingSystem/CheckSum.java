0b1d9515c017e645f3177cc28c743d96672b2f3e
lenardot <tel36@cornell.edu>
2013-03-30 14:26:11 -0300

    Update CheckSum.java
    Finished CheckSum

--- src/votingSystem/CheckSum.java
+++ src/votingSystem/CheckSum.java
@@ -2,9 +2,11 @@ package votingSystem;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class CheckSum {
 
+	private final static String HASH_FUNCTION = "SHA-256";
   /**
 	 * getCheckSum
 	 * Returns a SHA256 checksum for a given message
@@ -21,7 +23,7 @@ public class CheckSum {
 		
 		//digest the message and return that
 		try{
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			MessageDigest md = MessageDigest.getInstance(HASH_FUNCTION);
 			md.update(msg);
 			
 			return md.digest();
@@ -44,15 +46,61 @@ public class CheckSum {
 	 */
 	public static byte[] appendCheckSum(byte[] msg){
 		
+		//get the checksum for the message
 		byte[] checkSum = getCheckSum(msg);
+		
+		//make sure checksum calculation worked
 		if(checkSum == null){
 			return null;
 		}
+		//otherwise, copy the checksum and message into a new array
 		else{
 			byte[] toRet = new byte[msg.length + checkSum.length];
 			
+			//copy data into the new array
+			for(int i = 0; i < toRet.length; i++){
+				
+				if(i < checkSum.length){
+					toRet[i] = checkSum[i];
+				}
+				else{
+					toRet[i] = msg[(i - checkSum.length)];
+				}
+			}
+			
+			return toRet;
+		}
+
+	}
+	
+	/**
+	 * checkCheckSum
+	 * Determines whether or not a given checkSum is correct for a given message
+	 * 
+	 * @param checksum - The checksum to check
+	 * @param msg - the message to check the checkSum of
+	 * 
+	 * @return - true or false depending on whether or not the checkSum is valid
+	 */
+	public static boolean checkCheckSum(byte[] checksum, byte[] msg){
+		
+		//sanity check
+		if(msg == null || checksum == null){
+			return false;
 		}
 		
+		//digest the message and return whether or not the digest equals the 
+		try{
+			MessageDigest md = MessageDigest.getInstance(HASH_FUNCTION);
+			md.update(msg);
+				
+			return Arrays.equals(checksum, md.digest());	
+		}	
+		//on exception, print stack and return false
+		catch(NoSuchAlgorithmException e){
+			e.printStackTrace();
+			return false;
+		}
 		
 	}
 }
