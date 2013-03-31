bec7f542cc0025554f3a9560bae3bc667e8ef821
Clover <crb89@cornell.edu>
2013-03-31 12:04:26 -0400

    Corrected spelling mistake.

--- src/votingSystem/DigitalSignature.java
+++ src/votingSystem/DigitalSignature.java
@@ -4,8 +4,18 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
 
+/**
+ * Digital Message Signing
+ * @author Clover
+ */
 public class DigitalSignature {
 
+	/**
+	 * Signs a message using an RSA private key.
+	 * @param msg, the message to be signed
+	 * @param key, the private key used to sign
+	 * @return the signed message if successful, null otherwise
+	 */
 	public static byte[] signMessage(byte[] msg, PrivateKey key) {
 		try {
 			Signature sig = Signature.getInstance(Constants.SIG_ALG);
@@ -22,6 +32,14 @@ public class DigitalSignature {
 		return null;
 	}
 	
+	/**
+	 * Verifies a digitally signed message using the corresponding public key to the private key that
+	 * should have been used to sign the message.
+	 * @param msg, the digitally signed message to verify
+	 * @param key, the public key to verify with
+	 * @return the unsigned message
+	 * @throws InvalidSignatureException
+	 */
 	public static byte[] verifySignature(byte[] msg, PublicKey key) throws InvalidSignatureException {
 		try {
 			Signature sig = Signature.getInstance(Constants.SIG_ALG);
