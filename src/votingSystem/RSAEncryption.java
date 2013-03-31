d26d2800159428ef8a2cbaea1ce7f11c31d9635f
Clover <crb89@cornell.edu>
2013-03-31 13:49:34 -0400

    Commented and updated.

--- src/votingSystem/RSAEncryption.java
+++ src/votingSystem/RSAEncryption.java
@@ -1,7 +1,7 @@
 package votingSystem;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -14,6 +14,10 @@ import java.security.InvalidKeyException;
  */
 public class RSAEncryption {
 	
+	/**
+	 * Generates an RSA public-private key pair.
+	 * @return RSA public-private key pair
+	 */
 	public static KeyPair genKeys() {
 		try {
 			KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
@@ -23,30 +27,61 @@ public class RSAEncryption {
 			e.printStackTrace();
 		}
 		return null;
-}
+	}
 	
-	public static byte[] encrypt(byte[] msg, Key key) throws InvalidKeyException {
+	/**
+	 * Encrypts a message using RSA.
+	 * @param msg, the message to be encrypted
+	 * @param key, the key to use for encryption
+	 * @return the encrypted message
+	 * @throws InvalidKeyException
+	 * @throws BadPaddingException
+	 */
+	public static byte[] encrypt(byte[] msg, Key key) throws InvalidKeyException, BadPaddingException {
 		try {
 			Cipher cipher = Cipher.getInstance(Constants.RSA_ALG);
 		    cipher.init(Cipher.ENCRYPT_MODE, key);
 		    return cipher.doFinal(msg);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-		return null;
-	}
-	
-	public static byte[] decrypt(byte[] msg, Key key) throws InvalidKeyException {
-		try {
-			Cipher cipher = Cipher.getInstance(Constants.RSA_ALG);
-	        cipher.init(Cipher.DECRYPT_MODE, key);
-	        return cipher.doFinal(msg);
+		} catch (InvalidKeyException e) {
+			throw new InvalidKeyException();
+		} catch (BadPaddingException e) {
+			throw new BadPaddingException();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
+	/**
+	 * Decrypts an RSA encrypted message.
+	 * @param msg, the message to be decrypted
+	 * @param key, the key to use for decrypting
+	 * @return the decrypted message
+	 * @throws InvalidKeyException
+	 * @throws BadPaddingException
+	 */
+	public static byte[] decrypt(byte[] msg, Key key) throws InvalidKeyException, BadPaddingException {
+		try {
+			Cipher cipher = Cipher.getInstance(Constants.RSA_ALG);
+	        cipher.init(Cipher.DECRYPT_MODE, key);
+	        return cipher.doFinal(msg);
+		} catch (InvalidKeyException e) {
+			throw new InvalidKeyException();
+		} catch (BadPaddingException e) {
+			throw new BadPaddingException();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Same functionality as encrypt, but without padding. Necessary for oblivious transfer to work.
+	 * @param msg, the message to be encrypted
+	 * @param key, the key to use for encryption
+	 * @return the encrypted message
+	 * @throws InvalidKeyException
+	 */
 	public static byte[] encryptNoPadding(byte[] msg, Key key) throws InvalidKeyException {
 		try {
 			Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding");
@@ -58,6 +93,13 @@ public class RSAEncryption {
 		return null;
 	}
 	
+	/**
+	 * Same functionality as decrypt, but without padding. Necessary for oblivious transfer to work.
+	 * @param msg, the message to be decrypted
+	 * @param key, the key to use for decrypting
+	 * @return the decrypted message
+	 * @throws InvalidKeyException
+	 */
 	public static byte[] decryptNoPadding(byte[] msg, Key key) throws InvalidKeyException {
 		try {
 			Cipher cipher = Cipher.getInstance("RSA/ECB/NoPadding");
