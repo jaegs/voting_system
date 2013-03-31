ee24eb6b853a8d3b3150cd8098862b3a6d789c88
Clover <crb89@cornell.edu>
2013-03-31 13:44:44 -0400

    Added test.

--- src/votingSystem/test/RSAEncryptionTest.java
+++ src/votingSystem/test/RSAEncryptionTest.java
@@ -13,6 +13,7 @@ import javax.crypto.NoSuchPaddingException;
 
 import java.util.Arrays;
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class RSAEncryptionTest {
@@ -34,5 +35,40 @@ public class RSAEncryptionTest {
 		System.out.println("Decrypted Message Bytes: " + Arrays.toString(decryptedMsg));
 		
 		assertTrue(Arrays.equals(msg,decryptedMsg));
+	}
+	
+	@Test
+	public void testInterceptor() throws InvalidKeyException, BadPaddingException {
+		System.out.println();
+		System.out.println("Test showing that an intercepter cannot successfully decrypt the encrypted message:");
+		
+		String s = "Hello, World!";
+		byte[] msg = s.getBytes();
+		System.out.println("Message: " + s);
+		System.out.println("Message Bytes: " + Arrays.toString(msg));
+		
+		// Intended recipient's key pair
+		KeyPair keys = RSAEncryption.genKeys();
+		// Interceptor's key pair
+		KeyPair keys2 = RSAEncryption.genKeys();
+		
+		byte[] encryptedMsg = RSAEncryption.encrypt(msg, keys.getPublic());
+		System.out.println("Encrypted Message: " + new String(encryptedMsg));
+		System.out.println("Encrypted Message Bytes: " + Arrays.toString(encryptedMsg));
+		
+		try {
+			// Interceptor tries to decrypt with his private key
+			byte[] decryptedMsg = RSAEncryption.decrypt(encryptedMsg,keys2.getPrivate());
+			System.out.println("Decrypted Message: " + new String(decryptedMsg));
+			System.out.println("Decrypted Message Bytes: " + Arrays.toString(decryptedMsg));
+			assertTrue(Arrays.equals(msg,decryptedMsg));
+		} catch (InvalidKeyException e) {
+			System.out.println("Intercepter could not successfully decrypt the encrypted message.");
+			assertTrue(true);
+		} catch (BadPaddingException e) { 
+			// BadPaddingException is almost always because of an improper decrypt key
+			System.out.println("Intercepter could not successfully decrypt the encrypted message.");
+			assertTrue(true);
+		}
 	}
 }
