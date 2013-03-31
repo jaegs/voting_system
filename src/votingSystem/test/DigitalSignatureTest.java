8758d53a06bb75178808f01c74c500d08c275b37
Clover <crb89@cornell.edu>
2013-03-31 12:23:05 -0400

    Updated.

--- src/votingSystem/test/DigitalSignatureTest.java
+++ src/votingSystem/test/DigitalSignatureTest.java
@@ -1,33 +1,33 @@
 package votingSystem.test;
 
 import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
-import org.junit.Test;
-
-import votingSystem.Constants;
 import votingSystem.DigitalSignature;
 import votingSystem.InvalidSignatureException;
+import votingSystem.RSAEncryption;
 
 public class DigitalSignatureTest {
 	@Test	
 	public void testSigning() throws NoSuchAlgorithmException, InvalidSignatureException {
 		String s = "Hello, world!";
 		byte[] msg = s.getBytes();
-		System.out.println("Message: " + Arrays.toString(msg));
+		System.out.println("Message: " + s);
+		System.out.println("Message Bytes: " + Arrays.toString(msg));
 		
-		KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
-		keygen.initialize(Constants.RSA_KEY_SIZE);
-		KeyPair keys = keygen.genKeyPair();
+		KeyPair keys = RSAEncryption.genKeys();
 		
 		byte[] signedMsg = DigitalSignature.signMessage(msg, keys.getPrivate());
-		System.out.println("Signed Message: " + Arrays.toString(signedMsg));
+		System.out.println("Signed Message: " + new String(signedMsg));
+		System.out.println("Signed Message Bytes: " + Arrays.toString(signedMsg));
+		
 		byte[] verifiedMsg = DigitalSignature.verifySignature(signedMsg, keys.getPublic());
-		System.out.println("Verified Message: " + Arrays.toString(verifiedMsg));
+		System.out.println("Verified Message: " + new String(verifiedMsg));
+		System.out.println("Verified Message Bytes: " + Arrays.toString(verifiedMsg));
 		
 		assertTrue(Arrays.equals(msg, verifiedMsg));
 	}
