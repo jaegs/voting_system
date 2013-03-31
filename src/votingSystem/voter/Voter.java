d342ffe7a906ef73347cd462ae0f750c3061b11f
lenardot <tel36@cornell.edu>
2013-03-31 14:08:34 -0300

    Update Voter.java

--- src/votingSystem/voter/Voter.java
+++ src/votingSystem/voter/Voter.java
@@ -3,6 +3,7 @@ package votingSystem.voter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.net.UnknownHostException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
