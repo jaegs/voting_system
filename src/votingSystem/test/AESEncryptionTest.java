94f22e19307e052d922759e61d295ff0557f18b4
Clover <crb89@cornell.edu>
2013-03-31 12:22:37 -0400

    Updated.

--- src/votingSystem/test/AESEncryptionTest.java
+++ src/votingSystem/test/AESEncryptionTest.java
@@ -2,6 +2,7 @@ package votingSystem.test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -20,11 +21,22 @@ import votingSystem.RSAEncryption;
 
 public class AESEncryptionTest {
 	@Test	
-	public void testEncryptDecrypt() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidParameterSpecException {
-		KeyPair keys = RSAEncryption.genKeys();
+	public void testEncryptDecrypt() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidParameterSpecException, UnsupportedEncodingException {
 		String s = "I am testing a long string to see if AES works, because if it doesn't, I will cry very very much. Please work. Don't make me cry.";
-		byte[] msgEnc = AESEncryption.encrypt(s.getBytes(), keys.getPublic());
-		byte[] msgDec = AESEncryption.decrypt(msgEnc, keys.getPrivate());
-		assertTrue(Arrays.equals(s.getBytes(),msgDec));						
+		byte[] msg = s.getBytes();
+		System.out.println("Message: " + s);
+		System.out.println("Message Bytes: " + Arrays.toString(msg));
+		
+		KeyPair keys = RSAEncryption.genKeys();
+		
+		byte[] encryptedMsg = AESEncryption.encrypt(s.getBytes(), keys.getPublic());
+		System.out.println("Encrypted Message: " + new String(encryptedMsg));
+		System.out.println("Encrypted Message Bytes: " + Arrays.toString(encryptedMsg));
+		
+		byte[] decryptedMsg = AESEncryption.decrypt(encryptedMsg, keys.getPrivate());
+		System.out.println("Decrypted Message: " + new String(decryptedMsg));
+		System.out.println("Decrypted Message Bytes: " + Arrays.toString(decryptedMsg));		
+		
+		assertTrue(Arrays.equals(msg,decryptedMsg));						
 	}
 }
