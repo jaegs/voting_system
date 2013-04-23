d8428560f9c93ef1c37c557f05d4f6b08046d19c
Clover <crb89@cornell.edu>
2013-04-23 01:57:34 -0400

    Password erasure.

--- src/votingSystem/voter/Terminal.java
+++ src/votingSystem/voter/Terminal.java
@@ -3,6 +3,7 @@ package votingSystem.voter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Map;
@@ -38,8 +39,14 @@ public class Terminal {
 			System.out.println("Please enter your username");
 			String username = br.readLine();
 			System.out.println("Please enter your password");
-			String password = br.readLine();
-			v.willVote(username, password);
+			byte[] password = br.readLine().getBytes();
+			System.out.println("Entered Password: " + new String(password));
+			v.willVote(username, new String(password));
+			
+			// "Erase" password by overwriting stored password with random bytes
+			SecureRandom sr = new SecureRandom();
+			sr.nextBytes(password);
+			System.out.println("Erased Password: " + new String(password));
 			
 			if(!v.isVoting()) {
 				System.out.println("Sorry " + v.getName() + ", at this time we could not confirm your voting status.");
