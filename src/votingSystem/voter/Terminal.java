9c1052fd0a9a3abcb2cd7e632adfe09cd30d181d
Clover <crb89@cornell.edu>
2013-04-28 13:26:44 -0400

    Update

--- src/votingSystem/voter/Terminal.java
+++ src/votingSystem/voter/Terminal.java
@@ -1,7 +1,6 @@
 package votingSystem.voter;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.SecureRandom;
